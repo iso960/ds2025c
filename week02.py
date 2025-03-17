1a222cb88e431fe1aa959ce918322996de8d1761
parks <iso960@naver.com>
2025-03-17 15:32:10 +0900

    Guess game on py fixed

--- week02.py
+++ week02.py
@@ -4,11 +4,12 @@ rnd = random.randint(1, 100)
 win = False
 
 for i in range(7):
-    print(f"{7 - i}번의 기회가 남았습니다. 숫자 입력(1 ~ 100) : ")
+    print(f"{7 - i}번의 기회가 남았습니다. 숫자 입력(1 ~ 100) : ", end = '')
     answer = int(input())
     if answer == rnd:
         print("정답입니다!")
         win = True
+        break
     elif answer > rnd:
         print("입력하신 수는 정답보다 큰 수 입니다.")
     else:
