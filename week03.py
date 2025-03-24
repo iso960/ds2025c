d4b52594a80461e4626f6948367581b22feafcf7
parks <iso960@naver.com>
2025-03-24 14:53:31 +0900

    v0.7 array

--- week03.py
+++ week03.py
@@ -1,19 +1,6 @@
-import array
+groups = ['HOT', 'Seventeen', 'Black Pink', 'XDZ']
+# ratings = [4, 3, 2, 1, 100]
+ratings = [4, 3, 2, 1]
 
-def move_zeros(a_list):
-    zero_index = 0;
-    for index, n in enumerate(a_list):
-        if n != 0:
-            a_list[zero_index] = n
-            if zero_index != index:
-                a_list[index] = 0
-            zero_index += 1
-    return(a_list)
-
-arr = array.array('i', [99, 0, -7, 0, 16])
-move_zeros(arr)
-for i in range(len(arr)):
-    print(f"{arr[i]:5} {id(arr[i])}")
-
-l = [9, 9]
-print(id(0), id(1))
+group_rating = list(zip(groups, ratings)) # 크기가 작은 것을 기준으로 함
+print(group_rating)
