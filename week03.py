518d4a41a7e54e29b36c790f5fe12b9f07c49dbd
parks <iso960@naver.com>
2025-03-24 14:56:36 +0900

    0.8 array

--- week03.py
+++ week03.py
@@ -1,6 +1,3 @@
-groups = ['HOT', 'Seventeen', 'Black Pink', 'XDZ']
-# ratings = [4, 3, 2, 1, 100]
-ratings = [4, 3, 2, 1]
-
-group_rating = list(zip(groups, ratings)) # 크기가 작은 것을 기준으로 함
-print(group_rating)
+cities = ['Suwon', 'Hwasung', 'Incheon', 'Incheon', 'Bucheon', 'Incheon', 'Seoul']
+cities = set(cities)
+print(cities)
