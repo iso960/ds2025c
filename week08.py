b36398e0cdcd298aa221c5a5ca9352a8c08c02f4
parks <iso960@naver.com>
2025-04-28 15:31:43 +0900

    v3.4 tree

--- week08.py
+++ week08.py
@@ -1,50 +1,40 @@
-def pre_order(node):
-    if node:
-        print(node.data, end='-')
-        pre_order(node.left)
-        pre_order(node.right)
-
-def in_order(node):
-    if node:
-        in_order(node.left)
-        in_order(node.right)
-        print(node.data, end='-')
-
-def post_order(node):
-    if node:
-        post_order(node.left)
-        post_order(node.right)
-        print(node.data, end='-')
-
 class TreeNode:
     def __init__(self):
         self.left = None
         self.data = None
         self.right = None
 
-node1 = TreeNode()
-node1.data = 'hs'
+def post_order(node):
+    if node:
+        post_order(node.left)
+        post_order(node.right)
+        print(node.data, end='->')
 
-node2 = TreeNode()
-node2.data = 'sl'
-node1.left = node2
+if __name__ == "__main__":
+    numbers = [10, 15, 8, 3, 9]
+    root = None
 
-node3 = TreeNode()
-node3.data = 'mb'
-node1.right = node3
+    node = TreeNode()
+    node.data = numbers[0]
+    root = node
 
-node4 = TreeNode()
-node4.data = 'hw'
-node2.left = node4
+    # 2번째 원소부터 마지막 원소까지
+    for number in numbers[1:]:
+        node = TreeNode()
+        node.data = number
+        current = root
+        while True:
+            if number < current.data:
+                if current.left is None:
+                    current.left = node
+                    break
+                current = current.left # move
+            else:
+                if current.right is None:
+                    current.right = node
+                    break
+                current = current.right
 
-node5 = TreeNode()
-node5.data = 'zz'
-node2.right = node5
+    print("BST 구성 완료")
 
-node6 = TreeNode()
-node6.data = 'sm'
-node3.left = node6
-
-post_order(node1)
-print()
-pre_order(node1)
+    post_order(root)
