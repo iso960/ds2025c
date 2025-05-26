010584a43495f25e75fc80d780ff4c7c56ae7e83
parks <iso960@naver.com>
2025-05-26 14:15:15 +0900

    v4.7 graph

--- week12.py
+++ week12.py
@@ -1,3 +1,5 @@
+from collections import deque  # append, popleft
+
 graph = [
     [0, 1, 1, 0, 0, 0, 0, 0],
     [1, 0, 0, 1, 0, 0, 0, 0],
@@ -11,16 +13,26 @@ graph = [
 
 def dfs(g, i, visited):
     visited[i] = 1
-    print(chr(ord('A') + i), end = ' ')
+    print(chr(ord('A') + i), end=' ')
     for j in range(len(graph)):
         if g[i][j] == 1 and not visited[j]:
             dfs(g, j, visited)
 
+
 def bfs(g, i, visited):
-    pass
+    q = deque([i])
+    visited[i] = 1
+    while q:
+        i = q.popleft()
+        print(chr(ord('A') + i), end=' ')
+        for j in range(len(graph)):
+            if g[i][j] == 1 and not visited[j]:
+                q.append(j)
+                visited[j] = 1
+
 
 visited_dfs = [0 for _ in range(len(graph))]
 visited_bfs = [0 for _ in range(len(graph))]
 dfs(graph, 4, visited_dfs)
 print()
-bfs(graph, 5, visited_bfs)
+bfs(graph, 4, visited_bfs)
